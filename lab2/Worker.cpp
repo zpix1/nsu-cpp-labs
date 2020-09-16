66489340804f9a0796365b116de2c69eb56165e4
Ivan Baksheev <zpix-dev@list.ru>
2020-09-16 12:21:26 +0700

    lab2: all workers implemented

--- lab2/Worker.cpp
+++ lab2/Worker.cpp
@@ -1,6 +1,7 @@
 #include "Worker.h"
 
 #include <fstream>
+#include <algorithm>
 
 void ReadfileWorker::run_operation(Context& context) {
     auto input = std::ifstream(context.arguments[0]);
@@ -10,6 +11,40 @@ void ReadfileWorker::run_operation(Context& context) {
     }
 }
 
-void ReadfileWorker::run_operation(Context& context) {
+void WritefileWorker::run_operation(Context& context) {
+    auto output = std::ofstream(context.arguments[0]);
+    for (auto str: context.text) {
+        output << str << std::endl;
+    }
+}
 
+void GrepWorker::run_operation(Context& context) {
+    auto grepstr = context.arguments[0];
+    TextContainer new_text;
+    for (auto str: context.text) {
+        if (str.find(grepstr) != std::string::npos) {
+            new_text.push_back(str);
+        }
+    }
+    context.text = new_text;
+}
+
+void SortWorker::run_operation(Context& context) {
+    std::sort(context.text.begin(), context.text.end());
+}
+
+void ReplaceWorker::run_operation(Context& context) {
+    auto replace_from = context.arguments[0];
+    auto replace_to = context.arguments[1];
+    
+    for (auto str: context.text) {
+        str.replace(s.find(replace_from), replace_from.length(), replace_to);
+    }
+}
+
+void DumpWorker::run_operation(Context& context) {
+    auto output = std::ofstream(context.arguments[0]);
+    for (auto str: context.text) {
+        output << str << std::endl;
+    }
 }
