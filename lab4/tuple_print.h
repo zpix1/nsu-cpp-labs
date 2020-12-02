493d1c053caa0154b43d41f2be5f4de7928f0bd6
Ivan Baksheev <zpix-dev@list.ru>
2020-12-02 17:39:09 +0700

    lab4: minor changes in tuple_print.h

--- lab4/tuple_print.h
+++ lab4/tuple_print.h
@@ -8,10 +8,10 @@
 template<class... Types>
 std::ostream& operator<<(std::ostream& os, std::tuple<Types...> const& t) {
     std::apply([&os](Types const& ... args) {
-        os << '[';
+        os << '(';
         std::size_t n = 0;
         ((os << args << (++n != sizeof...(Types) ? ", " : "")), ...);
-        os << ']';
+        os << ')';
     }, t);
     return os;
 }
